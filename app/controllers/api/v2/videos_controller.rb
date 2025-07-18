47ef35a4ea5493015ad82fd7a4574051d5e363e1
bdblackhathacker <bd.blackhathacker35@gmail.com>
2025-07-18 19:41:41 +0600

    configure video controller

--- app/controllers/api/v2/videos_controller.rb
+++ app/controllers/api/v2/videos_controller.rb
@@ -1,2 +1,38 @@
-class Api::V2::VideosController < ApplicationController
+class Api::v2::VideosController < ApplicationController
+  include Rails.application.routes.url_helpers
+
+  def index
+    videos = Video.all.with_attached_file
+    render json: videos.map { |video| format_video(video) }
+  end
+
+  def show
+    video = Video.find(params[:id])
+    render json: format_video(video)
+  end
+
+  def create
+    video = Video.new(video_params)
+    if video.save
+      render json: format_video(video), status: :created
+    else
+      render json: { errors: video.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def video_params
+    params.permit(:title, :description, :file)
+  end
+
+  def format_video(video)
+    {
+      id: video.id,
+      title: video.title,
+      description: video.description,
+      video_url: url_for(video.file),
+      created_at: video.created_at
+    }
+  end
 end

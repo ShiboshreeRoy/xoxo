2cceab821b4b0695f2d69d16dfe1baf4a5d937f0
bdblackhathacker <bd.blackhathacker35@gmail.com>
2025-07-18 19:38:40 +0600

    configure  video  model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,7 @@
 class Video < ApplicationRecord
+    has_one_attached :file
+
+    validates :title, presence: true
+    validates :file, attached: true, content_type: ['video/mp4', 'video/mkv', 'video/webm']
+
 end
